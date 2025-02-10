87aba6dc3a51fcd006dedab895db7cc476f9e5e3
Felipe Loque <80802344+felipefonte@users.noreply.github.com>
2025-02-10 16:48:03 -0300

    small improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,22 @@ def main():
     search = st.secrets["SEARCH"]
     search = search.replace(" ", "+")
     search = search.upper()
+    
     # Get page
     driver.get(f'https://www.in.gov.br/consulta/-/buscar/dou?q=%22{search}%22&s=todos&exactDate=all&sortType=0')
-    # Get number of results
     driver.implicitly_wait(5)
-    numberResults = driver.find_element(By.CLASS_NAME, 'search-total-label')
-    numberResultsText = numberResults.text
-    print(numberResultsText)
-    # Show result
-    st.success(f'{numberResultsText}')
-
-    #with st.echo():
-    #st.code(driver.page_source)
+    
+    # Get number of results
+    numberResults = driver.find_element(By.CLASS_NAME, 'search-total-label').text
+    st.success(f'{numberResults}')
+    
+    # Get the latest result
+    place = driver.find_elements(By.CLASS_NAME, 'breadcrumb-item hierarchy-item-marker')[1].text
+    edition = driver.find_elements(By.CLASS_NAME, 'breadcrumb-item hierarchy-item-marker')[2].text
+    title = driver.find_elements(By.CLASS_NAME, 'title-marker')[0].text
+    st.success(f'Latest result: {place}, {edition}, {title}')
+    
+    # Quit
     driver.quit()
 
 
