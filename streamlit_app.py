3fd39553cc604c52f613ae1789ba76d46b421c84
Felipe Loque <80802344+felipefonte@users.noreply.github.com>
2025-02-10 15:43:05 -0300

    small improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
-
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+    
+def main():
 
     @st.cache_resource
     def get_driver():
@@ -24,5 +24,9 @@ with st.echo():
     driver = get_driver()
     driver.get("http://example.com")
 
+    #with st.echo():
     st.code(driver.page_source)
 
+
+if __name__ == '__main__':
+    main()
