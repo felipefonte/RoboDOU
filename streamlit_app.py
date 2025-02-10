09f9b60ec3e0eafd3f78b245b38df8539eb34cad
Felipe Loque <80802344+felipefonte@users.noreply.github.com>
2025-02-10 16:29:19 -0300

    small improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.common.by import By
     
 def main():
 
@@ -27,10 +28,19 @@ def main():
     search = st.secrets["SEARCH"]
     search = search.replace(" ", "+")
     search = search.upper()
+    # Get page
     driver.get(f'https://www.in.gov.br/consulta/-/buscar/dou?q=%22{search}%22&s=todos&exactDate=all&sortType=0')
+    # Get number of results
+    driver.implicitly_wait(5)
+    numberResults = driver.find_element(By.CLASS_NAME, 'search-total-label')
+    numberResultsText = numberResults.text
+    print(numberResultsText)
+    # Show result
+    st.success(f'{numberResultsText}')
 
     #with st.echo():
-    st.code(driver.page_source)
+    #st.code(driver.page_source)
+    driver.quit()
 
 
 if __name__ == '__main__':
