bf7bf998f8dc47e6e3bd5c99bf37eef18a3ed745
Felipe Loque <80802344+felipefonte@users.noreply.github.com>
2025-02-11 11:47:25 -0300

    small improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,21 +62,25 @@ def get_item_from_dou():
     driver.get(f'https://www.in.gov.br/consulta/-/buscar/dou?q=%22{search}%22&s=todos&exactDate=all&sortType=0')
     driver.implicitly_wait(5)
     
-    # Get number of results
-    numberResults = driver.find_element(By.CLASS_NAME, 'search-total-label').text
-    st.success(f'{numberResults}')
-    
-    # Get the latest result
-    place = driver.find_elements(By.CLASS_NAME, 'hierarchy-item-marker')[1].text
-    edition = driver.find_elements(By.CLASS_NAME, 'hierarchy-item-marker')[2].text
-    latest = driver.find_elements(By.CLASS_NAME, 'title-marker')[0]
-    latest_child = latest.find_element(By.TAG_NAME, 'a')
-    title = latest_child.text
-    url = latest_child.get_attribute('href')
-    latest_description = f'Último resultado:\n{place}\n{edition}\n{title}\n{url}'
-    st.success(latest_description)
-    
-    send_email(f'{numberResults}\n\n{latest_description}')
+    try:
+        # Get number of results
+        numberResults = driver.find_element(By.CLASS_NAME, 'search-total-label').text
+        st.success(f'{numberResults}')
+        
+        # Get the latest result
+        place = driver.find_elements(By.CLASS_NAME, 'hierarchy-item-marker')[1].text
+        edition = driver.find_elements(By.CLASS_NAME, 'hierarchy-item-marker')[2].text
+        latest = driver.find_elements(By.CLASS_NAME, 'title-marker')[0]
+        latest_child = latest.find_element(By.TAG_NAME, 'a')
+        title = latest_child.text
+        url = latest_child.get_attribute('href')
+        latest_description = f'Último resultado:\n{place}\n{edition}\n{title}\n{url}'
+        st.success(latest_description)
+
+        # Send email
+        send_email(f'{numberResults}\n\n{latest_description}')
+    except Exception as e:
+        st.error(f"Failed to read DOU: {e}")
 
 if __name__ == '__main__':
     get_item_from_dou()
